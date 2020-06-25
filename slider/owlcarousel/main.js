7a3af170efe865e1d5638bc970b2b00d434c597b
VietHoang <hoangcf1134@gmail.com>
2020-06-25 22:08:50 +0700

    This is my Project

---
+++ slider/owlcarousel/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    var owl = $('.owl-carousel');
+  owl.owlCarousel({
+      items:3,
+      loop:true,
+      margin:10,
+      autoplay:true,
+      autoplayTimeout:1000,
+      autoplayHoverPause:true
+  });
+  $('.play').on('click',function(){
+      owl.trigger('play.owl.autoplay',[1000])
+  })
+  $('.stop').on('click',function(){
+      owl.trigger('stop.owl.autoplay')
+  })
+  });
